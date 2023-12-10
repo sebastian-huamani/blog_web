b5cbabadd528ad9a74b8083e2eda8a356134eb08
sebastian-huamani <huamanitassara@gmail.com>
2023-12-10 15:51:55 -0500

    Ruta - parametros

--- src/index.js
+++ src/index.js
@@ -12,14 +12,26 @@ app.get('/home', (req, res) => {
     res.render('home');
 });
 
-app.get('/sobre-nosotros', (req, res) => {
+
+app.get('/sobre_nosotros', (req, res) => {
     res.render('sobre_nosotros');
 });
 
-
 app.get('/blog/:blogId', (req, res) => {
-    res.render('blog');
+    console.log(req.params);
+    res.render('blog', {blogId : req.params.blogId});
 });
+
+
+
+app.get('/iniciar_session', (req, res) => {
+    res.render('iniciar_session');
+});
+
+app.get('/registrarme', (req, res) => {
+    res.render('registrarme');
+});
+
 
 app.listen(3000);
 
