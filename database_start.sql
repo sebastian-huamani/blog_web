fc1445c1407e79646a04c31428bb9e7b303cda5b
sebastian-huamani <huamanitassara@gmail.com>
2023-12-16 10:58:22 -0500

    db conexion

---
+++ database_start.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE webpost;
+
+use webpost;
+
+create TABLE rol(
+    id int not null auto_increment primary key,
+    nombre varchar(255) not null
+);
+
+create table categoria(
+    id int not null auto_increment primary key,
+    nombre varchar(255) not null
+);
+
+CREATE TABLE usuario(
+    id int not null auto_increment primary key,
+    nombre varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    rol_id int not null,
+    constraint FK_usuario_rol FOREIGN KEY (rol_id) REFERENCES rol(id)
+);
+
+create TABLE post(
+    id int not null auto_increment primary key,
+    usuario_id int not null,
+    categoria_id int not null,
+    titulo varchar(255) not null,
+    cuerpo varchar(255) not null,
+    veces_leido int not null ,
+    me_gusta int not null,
+    constraint FK_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    constraint FK_post_categoria FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+);
+
+
+INSERT INTO categoria (id, nombre) VALUES
+(1, 'base de datos'),
+(2, 'Sql'),
+(3, 'MySQL'),
+(4, 'PHP'),
+(5, 'Spring Boot'),
+(6, 'Java'),
+(7, 'Css'),
+(8, 'Software'),
+(9, 'Framework'),
+(10, 'JavaScript');
+
+
+INSERT into rol (nombre) values ('admin'), ('usuario');
--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-const express = require('express');
-const path = require('path')
+import { pool } from './db.js'
+import express  from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/home', (req, res) => {
-    res.render('home');
-});
-
+app.get('/home', async (req, res) => {
+    const [result] = await pool.query('select * from producto');
+    // res.json(result);
+    res.render('home', {productos : result});
+}); 
 
 app.get('/sobre_nosotros', (req, res) => {
     res.render('sobre_nosotros');
-});
+}); 
 
 app.get('/blog/:blogId', (req, res) => {
     console.log(req.params);
     res.render('blog', {blogId : req.params.blogId});
+    
 });
 
 
